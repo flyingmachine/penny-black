2205f8feee2f8dd932b0bd0e2658d30e837d0375
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-12-03 20:27:01 -0500

    so close to working

--- penny-black-apache-commons/src/com/flyingmachine/penny_black_apache_commons.clj
+++ penny-black-apache-commons/src/com/flyingmachine/penny_black_apache_commons.clj
@@ -1,13 +1,12 @@
 (ns com.flyingmachine.penny-black-apache-commons
-  (:require com.flyingmachine.penny-black.core.send
+  (:require [com.flyingmachine.penny-black.core.send :refer (send-with-backend)]
             [com.flyingmachine.penny-black.core.config :refer (config)])
   (:import org.apache.commons.mail.HtmlEmail))
-
 
 (defmethod send-with-backend :default [params]
   (do
     (doto (HtmlEmail.)
-      (.setHostName (config :hostname))
+      (.setHostName (config :host))
       (.setSslSmtpPort (config :ssl-smtp-port))
       (.setSSL (config :use-ssl))
       (.addTo (:to params))
--- penny-black-apache-commons/test/com/flyingmachine/penny_black_apache_commons_test.clj
+++ penny-black-apache-commons/test/com/flyingmachine/penny_black_apache_commons_test.clj
@@ -1,6 +1,9 @@
 (ns com.flyingmachine.penny-black-apache-commons-test
-  (:use clojure.test
-        com.flyingmachine.penny-black-apache-commons))
+  (:require [com.flyingmachine.penny-black.core.config :refer (config)]
+            [com.flyingmachine.penny-black.core.send :as send]
+            [com.flyingmachine.penny-black.core.templates :as templates]
+            com.flyingmachine.penny-black-apache-commons)
+  (:use midje.sweet))
 
 (def template-vars
   {:var1 "a"
@@ -9,8 +12,8 @@
    :htmlvar2 "<p>b</p>"})
 
 (def mail-options
-  {:from (get-in env [:com-flyingmachine-penny-black :test-from])
-   :to (get-in env [:com-flyingmachine-penny-black :test-to])
+  {:from (config :test-from)
+   :to (config :test-to)
    :subject "penny black test"
    :body (templates/body "test" template-vars)})
 
@@ -18,4 +21,4 @@
   (send/send-email*
    true
    mail-options)
-  => mail-options)
+  => true)
