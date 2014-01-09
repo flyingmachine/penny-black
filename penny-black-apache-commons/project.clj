4e30882e610df4cb9827c80881eae0a66d44743f
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2014-01-09 18:52:37 -0500

    version bump

--- penny-black-apache-commons/project.clj
+++ penny-black-apache-commons/project.clj
@@ -1,4 +1,4 @@
-(defproject com.flyingmachine/penny-black-apache-commons "0.1.0"
+(defproject com.flyingmachine/penny-black-apache-commons "0.1.1"
   :description "Apache Commons email backend for penny black"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
