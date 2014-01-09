3e9442e6f5af59d4e2dd68249cb9e4a6a7504d39
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2014-01-09 18:51:00 -0500

    fix issue with optional text/html

--- penny-black-apache-commons/src/com/flyingmachine/penny_black_apache_commons.clj
+++ penny-black-apache-commons/src/com/flyingmachine/penny_black_apache_commons.clj
@@ -5,17 +5,20 @@
 
 (defmethod send-with-backend :default [params]
   (do
-    (doto (HtmlEmail.)
-      (.setHostName (config :host))
-      (.setSslSmtpPort (config :ssl-smtp-port))
-      (.setSSL (config :use-ssl))
-      (.addTo (:to params))
-      (.setFrom (config params :from-address) (config params :from-name))
-      (.setSubject (:subject params))
-      (.setTextMsg (get-in params [:body :text]))
-      (.setHtmlMsg (get-in params [:body :html]))
-      (.setAuthentication (config params :authentication :username)
-                          (config params :authentication :password))
-      (.send))
+    (let [email (HtmlEmail.)]
+      (doto email
+        (.setHostName (config :host))
+        (.setSslSmtpPort (config :ssl-smtp-port))
+        (.setSSL (config :use-ssl))
+        (.addTo (:to params))
+        (.setFrom (config params :from-address) (config params :from-name))
+        (.setSubject (:subject params))
+        (.setAuthentication (config params :authentication :username)
+                            (config params :authentication :password)))
+      (if-let [text (get-in params [:body :text])]
+        (.setTextMsg email text))
+      (if-let [html (get-in params [:body :html])]
+        (.setHtmlMsg email html))
+      (.send email))
     true))
 
